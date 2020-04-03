825b7cf4c7574390452fb6a8cc27413d72146056
KLisabeth <professional.translations4ppl@gmail.com>
2020-04-03 20:06:29 +0200

    remove stored books listener

--- listeners/delete-book.js
+++ listeners/delete-book.js
@@ -1,5 +1,6 @@
 // Event: Remove Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     deleteBook(e.target);
+    store.removeBook(e.target.parentElement.previousElementSibling.textContent);
     showAlert('Book Removed', 'success');
 });
